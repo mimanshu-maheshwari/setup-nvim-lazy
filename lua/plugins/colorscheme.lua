c896584eb72ed17cde35490d97535b834d99f0b1
mimanshu-maheshwari <maheshwarimimanshu@gmail.com>
2026-07-22 14:50:01 +0530

    UPdate colorscheme.lua

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,17 +1,32 @@
 return {
-	"folke/tokyonight.nvim",
-	lazy = false,
-	priority = 1000,
-	opts = {
-		transparent = true, -- Enables transparency for the main editor window
-		styles = {
-			-- sidebars = "transparent", -- Makes sidebars (like neo-tree, nvim-tree) transparent
-			-- floats = "transparent",   -- Makes floating windows transparent
-		},
-	},
-	config = function(_, opts)
-		require("tokyonight").setup(opts)
-		vim.cmd([[colorscheme tokyonight]])
-	end,
+  "tinted-theming/tinted-vim",
+  lazy = false,
+  priority = 1000,
+  opts = {
+    transparent = true, -- Enables transparency for the main editor window
+    styles = {
+      -- sidebars = "transparent", -- Makes sidebars (like neo-tree, nvim-tree) transparent
+      -- floats = "transparent",   -- Makes floating windows transparent
+    },
+  },
+  config = function(_, opts)
+    -- require("tokyonight").setup(opts)
+    vim.cmd([[colorscheme base16-atelier-forest]])
+  end,
 }
-
+-- return {
+--   "folke/tokyonight.nvim",
+--   lazy = false,
+--   priority = 1000,
+--   opts = {
+--     transparent = true, -- Enables transparency for the main editor window
+--     styles = {
+--       -- sidebars = "transparent", -- Makes sidebars (like neo-tree, nvim-tree) transparent
+--       -- floats = "transparent",   -- Makes floating windows transparent
+--     },
+--   },
+--   config = function(_, opts)
+--     require("tokyonight").setup(opts)
+--     vim.cmd([[colorscheme tokyonight]])
+--   end,
+-- }
